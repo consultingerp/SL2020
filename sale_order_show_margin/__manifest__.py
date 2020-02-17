82833ec1f26aeea518fe03f105d33888c8bede1c
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2020-02-17 12:56:33 +0200

    module/sale_order_show_margin uploaded

---
+++ sale_order_show_margin/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Order Show Margin ',
+    'version': '1.0',
+    'depends': ['base','sale','product','hr','contacts'],
+    'data': [
+         'views/sale_order.xml',
+    ],
+    'installable': True,
+}
---
+++ sale_order_show_margin/models/sale_order_cost.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.addons import decimal_precision as dp
+
+
+
+
+class SaleOrderCost(models.Model):
+    _inherit  = 'sale.order'
+
+    is_cost_show = fields.Boolean('Display Margin', help='Indicates whether to show Margin and Cost or not.')
+
+
+
+
+
