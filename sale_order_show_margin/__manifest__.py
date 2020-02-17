6cb3507c85b7a94820e0daed3bbda1e1a9ff3a18
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2020-02-17 19:21:36 +0200

    Update __manifest__.py

--- sale_order_show_margin/__manifest__.py
+++ sale_order_show_margin/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Sale Order Show Margin ',
     'version': '1.0',
-    'depends': ['base','sale','product','hr','contacts'],
+    'depends': ['base','sale','product','hr','contacts','sale_margin'],
     'data': [
          'views/sale_order.xml',
     ],
