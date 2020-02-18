0a4317769f9f7e3180580bc8132eba04fe1c4f7f
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2020-02-18 09:59:27 +0200

    module: sale_order_show_margin updated

--- sale_order_show_margin/models/sale_order_cost.py
+++ sale_order_show_margin/models/sale_order_cost.py
@@ -6,8 +6,12 @@ from odoo.addons import decimal_precision as dp
 
 
 class SaleOrderCost(models.Model):
-    _inherit  = 'sale.order'
+    _inherit  = 'sale.order.line'
 
+    is_cost_show_line = fields.Boolean('Display Margin', help='Indicates whether to show Margin and Cost or not.')
+
+class SaleOrderMargin(models.Model):
+    _inherit = "sale.order"
     is_cost_show = fields.Boolean('Display Margin', help='Indicates whether to show Margin and Cost or not.')
 
 
